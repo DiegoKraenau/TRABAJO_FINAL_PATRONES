33633a17fecac41eb2aa2b76d86e05d48efa8d22
Adrian992 <adrian_30escorpio99@hotmail.com>
2019-06-02 23:11:49 -0500

    conexion

--- TFPatrones/src/Codigo/ConexionUPConsulta.java
+++ TFPatrones/src/Codigo/ConexionUPConsulta.java
@@ -9,7 +9,7 @@ public class ConexionUPConsulta {
 		System.out.print("Conexion0 ");
 		try	{
 			System.out.print("Conexion1 ");
-			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			Class.forName("com.microsoft.jdbc.sqlserver.SQLServerDriver");
 			System.out.print("Conexion2 ");
 			cn=java.sql.DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=patronesTF","sa", "123456");
 			System.out.print("Conexion3 ");
